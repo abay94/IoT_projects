f1e94445cabced5bf1f32be95cb21d8c6fdc5d53
Abay Aubakirov <abay.aubakirov@nu.edu.kz>
2023-12-12 12:54:02 +0600

    Update smart_parking.ino

--- fgs/smart_parking.ino
+++ fgs/smart_parking.ino
@@ -1,12 +1,18 @@
 #include <Servo.h>
 
+// #define parking_led_1 5
 //inizialize micro servo 9g
 Servo servo;
+const int parking_led_1 = 4;
+const int pir1 = 2; // input pin (IR LED parking space 1)
+
 
 void setup() {
   
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  pinMode(parking_led_1, OUTPUT);
+  pinMode(pir1, INPUT);
 
   //attach the servo to pin 9
   servo.attach(9);
@@ -71,4 +77,21 @@ void loop() {
   }
 
   delay(500);
+
+
+
+  //parking space 1
+  int sensorOut1 = digitalRead(pir1);
+
+  if(sensorOut1==LOW){
+    digitalWrite(parking_led_1, LOW);
+    Serial.println("Parking space 1 FREE");
+  }
+  else{
+    digitalWrite(parking_led_1, HIGH);
+    Serial.println("Parking space 1 BUSY");
+  }
+  // delay(1000);
+
+
 }
